fa74f0f38ed9b27304495ac8095d98219f464677
lgf-136 <86616706+lgf-136@users.noreply.github.com>
2022-09-15 23:31:52 +0800

    2 files modified

--- src/components/Article/ArticleItem.tsx
+++ src/components/Article/ArticleItem.tsx
@@ -1,9 +1,83 @@
-function ArticleItem({ username, tags, title, date, url, content }) {
+import React, { useCallback, useEffect } from 'react';
+
+function ArticleItem({
+  username,
+  category,
+  clickCount,
+  isStar,
+  starCount,
+  tags,
+  title,
+  date,
+  url,
+  content,
+  updateArticle,
+}) {
+  //https://www.bilibili.com/video/BV15Y4y1z7Fc?p=7&vd_source=6cecd1f17a6c0ef08a944dd92199a516
+  useEffect(() => {
+    console.log('article: ', title, ' has been mounted!');
+    return () => {
+      console.log('article: ', title, ' has been unmounted!');
+    };
+  }, []);
+  useEffect(() => {
+    const data = {
+      username,
+      category,
+      clickCount,
+      isStar,
+      starCount,
+      tags,
+      title,
+      date,
+      url,
+      content,
+    };
+    console.log(
+      'article: ',
+      title,
+      ' has been updated! result data is: ',
+      data
+    );
+    return () => {
+      console.log('previous data is: ', data);
+    };
+  }, [
+    username,
+    category,
+    clickCount,
+    isStar,
+    starCount,
+    tags,
+    title,
+    date,
+    url,
+    content,
+  ]);
+  const handleClickCount = () => {
+    const updatedArticle = {
+      username: username,
+      category: category,
+      clickCount: clickCount + 1,
+      isStar: isStar,
+      starCount: starCount,
+      tags: tags,
+      title: title,
+      date: date,
+      url: url,
+      content: content,
+    };
+    updateArticle(updatedArticle);
+    console.log('aa');
+  };
   return (
     <article className="relative flex items-center transition-transform transform group hover:-translate-x-2">
       <div className="flex flex-col flex-grow py-8 space-y-4 text-base rounded px-8 shadow-md bg-off-base">
         <div className="flex flex-row justify-between">
           <span>{username}</span>
+          <span className="bg-base text-primary px-2 py-1 rounded cursor-pointer hover:bg-primary hover:text-base">
+            {category}
+          </span>
           <span className="space-x-1">
             {tags.map((tag, index) => {
               return (
@@ -17,11 +91,22 @@ function ArticleItem({ username, tags, title, date, url, content }) {
             })}
           </span>
         </div>
-        <div className="flex flex-row justify-between">
-          <h3 className="text-xl font-bold">{title}</h3>
+        <div className="flex flex-row justify-between gap-2">
+          <a href={url}>
+            <h3 className="text-xl font-bold" onClick={handleClickCount}>
+              {title}
+            </h3>
+          </a>
+
           <span className="text-muted">{date}</span>
         </div>
+
         <p className="max-w-3xl leading-8 text-muted">{content}</p>
+        <div className="flex flex-row gap-4">
+          <span className="text-secondary">阅读量 : {clickCount}</span>
+          <span className="text-secondary">收藏量 : {starCount}</span>
+          <button className="text-secondary">收藏 {isStar ? 1 : 0}</button>
+        </div>
       </div>
     </article>
   );
--- src/components/Article/index.tsx
+++ src/components/Article/index.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import ArticleItem from './ArticleItem';
 
 // typescript提示implicitly has an 'any' type，这个怎么解决？tsconfig.json 中 添加 "noImplicitAny": false,
@@ -5,6 +6,10 @@ import ArticleItem from './ArticleItem';
 export interface IArticle {
   [key: string]: any;
   username: string;
+  category: string;
+  clickCount: number;
+  isStar: boolean;
+  starCount: number;
   tags: string[];
   title: string;
   date: string;
@@ -13,9 +18,13 @@ export interface IArticle {
 }
 
 export default ({ theme }) => {
-  const articles:IArticle[] = [
+  const [articles, setArticles] = useState([
     {
-      username: 'userName',
+      username: 'lgf',
+      category: 'programming',
+      clickCount: 0,
+      isStar: false,
+      starCount: 0,
       tags: ['react', 'hooks'],
       title: 'useEffect 完整指南',
       date: '2020-06-28',
@@ -24,26 +33,33 @@ export default ({ theme }) => {
         '你用Hooks写了一些组件，甚或写了一个小型应用。你可能很满意，使用它的API很舒服并且在这个过程中获得了一些小技巧。',
     },
     {
-      username: 'userName',
+      username: 'adam',
+      category: 'programming',
+      clickCount: 0,
+      isStar: false,
+      starCount: 0,
       tags: ['css', 'tailwind'],
       title: '使用 CSS variables 和Tailwind css实现主题换肤',
       url: '#',
       date: '2020-06-30',
       content: '根据Tailwind Labs的[换肤视频]，手动实践。',
     },
-  ];
+  ]);
+  const updateArticle = (article) => {
+    setArticles([...articles, article]);
+  };
   return (
     <div className={`${theme} p-10 bg-base`}>
       <h2 className="mb-6 text-3xl font-semibold text-left text-primary">
         最近文章
       </h2>
       <ul className="space-y-10">
-        {articles.map((article, index) => {
+        {articles.map((article, index, articles) => {
           return (
             <li key={index}>
-              <a href={article.url}>
-                <ArticleItem {...article} />
-              </a>
+              {/* <a href={article.url}> */}
+              <ArticleItem {...article} {...updateArticle} />
+              {/* </a> */}
             </li>
           );
         })}
